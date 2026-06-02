beb800c06e4035e417ddb6ec25f82d7baf44471a
qyleron-dev <qyleron@gmail.com>
2026-06-02 18:30:55 +0530

    Add editable YAML rule evaluation

---
+++ classifier/rules/__init__.py
@@ -0,0 +1,13 @@
+from classifier.rules.engine import (
+    RuleEvaluation,
+    RuleMatch,
+    evaluate_rules,
+    load_rules,
+)
+
+__all__ = [
+    "RuleEvaluation",
+    "RuleMatch",
+    "evaluate_rules",
+    "load_rules",
+]
